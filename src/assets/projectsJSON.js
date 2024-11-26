5880d9afea7486eb72b8247e270bbf2d879b2c21
goktemkirez <ogkirez@gmail.com>
2024-11-26 11:50:40 +0300

    added new projects

--- src/assets/projectsJSON.js
+++ src/assets/projectsJSON.js
@@ -1,70 +1,128 @@
 export const projectsJson = [
   {
-    projectID: "1",
-    title: "Durak App",
+    projectID: "10",
+    title: "@goktemkirez/react-table (Npm Package)",
     detail:
-      "Durak App is a mobile application developed with React Native to help sales department for products\u2019 information and unit conversion",
+      "A table component that is useful for production companies.",
     videoUrl:
-      "https://res.cloudinary.com/dajbm4ku6/image/upload/v1644911219/goktemkirez.com/threadurak_mbc2ck.gif",
-    codeUrl: "https://github.com/goktemkirez/ThreaDurak",
+      "https://res.cloudinary.com/dajbm4ku6/image/upload/v1732607135/goktemkirez.com/react-table_etsraj.gif",
+    codeUrl: "https://www.npmjs.com/package/@goktemkirez/react-table",
     liveUrl:
-      "https://play.google.com/store/apps/details?id=com.duraktekstil.threadurakapp&gl=TR",
-    sorting: "1",
+      "https://www.npmjs.com/package/@goktemkirez/react-table",
+    sorting: "10",
   },
   {
-    projectID: "2",
-    title: "Temflix",
-    detail: "Netflix clone developed with React.js",
+    projectID: "20",
+    title: "Durak App (React Native)",
+    detail:
+      "Durak App is a mobile application developed with React Native to support sales staff for products information, unit conversion etc.. The app is on live on Play Store and iOS.",
     videoUrl:
-      "https://res.cloudinary.com/dajbm4ku6/image/upload/v1644911296/goktemkirez.com/temflix_isobsg.gif",
-    codeUrl: "https://github.com/goktemkirez/my-netflix-app",
-    liveUrl: "https://temflix.netlify.app/",
+      "https://res.cloudinary.com/dajbm4ku6/image/upload/v1732601732/goktemkirez.com/yeni-durak-app_xmplvw.gif",
+    codeUrl: "",
+    liveUrl:
+      "https://apps.apple.com/ng/app/durak-app/id6737593201?uo=2",
+    sorting: "20",
+  },
+  {
+    projectID: "30",
+    title: "DurakWeb (React.js & .NET 6)",
+    detail:
+      "DurakWeb aims to digitalize and simplify the processes of all departments within the company. It contains many modules such as video training platform, R&D, production support modules, HR performance management, sales and marketing applications, document management and live messaging.",
+    videoUrl:
+      "https://res.cloudinary.com/dajbm4ku6/image/upload/v1732609701/goktemkirez.com/durakweb_aly7gf.gif",
+    codeUrl: "",
+    liveUrl: "",
+    sorting: "30",
+  },
+  {
+    projectID: "40",
+    title: "BT Destek",
+    detail:
+      "BT Destek is an IT Support app which is developed with React.js on frontend, .NET 6 on backend. MSSQL is used as database. The app provides ticket sistem, device management and other IT requirements with a responsive design.",
+    videoUrl:
+      "https://res.cloudinary.com/dajbm4ku6/image/upload/v1732609366/goktemkirez.com/bt-destek_kiybww.gif",
+    codeUrl: "",
+    liveUrl: "",
     sorting: "40",
   },
   {
-    projectID: "6",
-    title: "DurakWeb",
+    projectID: "50",
+    title: "Dashboard Designer (Devexpress)",
+    detail:
+      "Dashboard Designer was developed to manage reports that appear as slides on LED screens in production. Dexexpress Dashboard was used to create dynamic reports and MUI was used to design a clean pages on slides.",
+    videoUrl:
+      "https://res.cloudinary.com/dajbm4ku6/image/upload/v1732608589/goktemkirez.com/dashboard-designer_p7arav.gif",
+    codeUrl: "",
+    liveUrl: "",
+    sorting: "50",
+  },
+  {
+    projectID: "60",
+    title: "Meeting Room Scheduler",
+    detail:
+      "This app which works integrated with Outlook, was developed with React.js and .NET 6 to track the occupancy of meeting rooms and the schedule of staff.",
+    videoUrl:
+      "https://res.cloudinary.com/dajbm4ku6/image/upload/v1732608588/goktemkirez.com/meeting-room_fdna4r.gif",
+    codeUrl: "",
+    liveUrl: "",
+    sorting: "60",
+  },
+  {
+    projectID: "70",
+    title: "Durak App (Old Version)",
+    detail:
+      "Durak App is a mobile application developed with React Native to help sales department for products information and unit conversion",
+    videoUrl:
+      "https://res.cloudinary.com/dajbm4ku6/image/upload/v1644911219/goktemkirez.com/threadurak_mbc2ck.gif",
+    codeUrl: "",
+    liveUrl:
+      "https://play.google.com/store/apps/details?id=com.duraktekstil.threadurakapp&gl=TR",
+    sorting: "70",
+  },
+  {
+    projectID: "80",
+    title: "DurakWeb (ASP.NET)",
     detail:
       "DurakWeb is a web application developed with ASP.NET webforms. Processes such as new project approving, product commissioning, KPI follow up, employee performance tracking, quality assurance manage on this portal",
     videoUrl:
       "https://res.cloudinary.com/dajbm4ku6/image/upload/v1644915163/goktemkirez.com/durakweb-5mb_vq2cyk.gif",
     codeUrl: "",
     liveUrl: "",
-    sorting: "300",
+    sorting: "80",
   },
   {
-    projectID: "7",
-    title: "Durak Mail App",
-    detail: "Durak Mail App is an auto mail sender app developed with .NET",
+    projectID: "90",
+    title: "Temflix",
+    detail: "Netflix clone developed with React.js",
+    videoUrl:
+      "https://res.cloudinary.com/dajbm4ku6/image/upload/v1644911296/goktemkirez.com/temflix_isobsg.gif",
+    codeUrl: "https://github.com/goktemkirez/my-netflix-app",
+    liveUrl: "https://temflix.netlify.app/",
+    sorting: "90",
+  },
+  {
+    projectID: "100",
+    title: "Mail Report Scheduler",
+    detail: "Mail Report Scheduler is only for sending Excel Reports feeding from SQL views. Developed with .NET",
     videoUrl:
       "https://res.cloudinary.com/dajbm4ku6/image/upload/v1644911245/goktemkirez.com/dbf7q8pgw4ocr61u0sxd.gif",
     codeUrl: "https://github.com/goktemkirez/AutoMailApp",
     liveUrl: "",
-    sorting: "500",
+    sorting: "100",
   },
   {
-    projectID: "8",
-    title: "myAPI",
-    detail: "ASP.NET Core Web API developed for goktemkirez.com",
-    videoUrl:
-      "https://res.cloudinary.com/dajbm4ku6/image/upload/v1659963126/goktemkirez.com/myAPI_mdseth.png",
-    codeUrl: "https://github.com/goktemkirez/myAPI",
-    liveUrl: "https://goktemkirez-api.herokuapp.com/api/Projects",
-    sorting: "10",
-  },
-  {
-    projectID: "14",
+    projectID: "110",
     title: "CoreDemo Blog App",
     detail:
-      "ASP.NET Core 6.0 app developed during a Youtube education series. N-Tier Architecture, EF Core, Identity, Web API, JWT are used in the project",
+      "ASP.NET Core 6.0 app developed during an education series. N-Tier Architecture, EF Core, Identity, Web API, JWT are used in the project",
     videoUrl:
       "https://res.cloudinary.com/dajbm4ku6/image/upload/v1659965667/goktemkirez.com/CoreDemo_rtzl4m.gif",
     codeUrl: "https://github.com/goktemkirez/CoreDemo",
-    liveUrl: "https://coredemo6.azurewebsites.net/",
-    sorting: "30",
+    liveUrl: "",
+    sorting: "110",
   },
   {
-    projectID: "15",
+    projectID: "120",
     title: "Hantasify",
     detail:
       "React app developed to learn API Token, data mapping, axios etc. Spotify API used in the app.",
@@ -72,6 +130,6 @@ export const projectsJson = [
       "https://res.cloudinary.com/dajbm4ku6/image/upload/v1659967233/goktemkirez.com/hantasify_oa4qtz.gif",
     codeUrl: "https://github.com/goktemkirez/hantasify",
     liveUrl: "https://hantasify.netlify.app/",
-    sorting: "20",
+    sorting: "120",
   },
 ];
